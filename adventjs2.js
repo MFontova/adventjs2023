3c23367868c5483353ecb341e6f37673365f5267
Marc Fontova <mfontovat@gmail.com>
2023-12-06 08:16:11 +0100

    adventjs 2 solution

--- adventjs2.js
+++ adventjs2.js
@@ -1,33 +1,27 @@
 const gifts = ['tren', 'oso', 'pelota']
 const materials = 'tronesa'
 
-manufacture(gifts, materials)
+console.log(manufacture(gifts, materials))
 
 function manufacture(gifts, materials) {
-  gifts.forEach(gift => {
-    var letters = gift.split('')
-    var possible = false
-    console.log(letters)
-    materials.split('').forEach(material => {
-      // console.log(typeof material)
-      var found = letters.find(letter => letter === material)
-      console.log('found', found)
-      if (found) {
-        possible = true
-      } else {
-        possible = false
-      }
-      // if (letters.include(material)) {
-      //   possible = true
-      // } else {
-      //   possible = false
-      // }
-    })
-    console.log(gift, possible)
-    // possible ? possibleGifts.push(gift) : null
-  });
   var possibleGifts = []
 
+  gifts.forEach(gift => {
+    var lettersArray = gift.split('')
+    var materialsArray = materials.split('')
 
+    var possibleArray = []
+
+    lettersArray.forEach(letter => {
+      if(materialsArray.includes(letter)) {
+        possibleArray.push(letter)
+      } else {
+      }
+    })
+    if(possibleArray.join('') === gift) {
+      possibleGifts.push(gift)
+    }
+  });
+  
   return possibleGifts
 }
