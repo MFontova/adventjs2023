1495f0bb48df5bb190b05b46e0cf549e9984f4d9
Marc Fontova <mfontovat@gmail.com>
2023-12-12 09:38:46 +0100

    solution 11

--- adventjs11.ts
+++ adventjs11.ts
@@ -1,13 +1,26 @@
-console.log(getIndexsForPalindrome('anna')) // []
-console.log(getIndexsForPalindrome('abab')) // [0, 1]
-console.log(getIndexsForPalindrome('abac')) // null
-console.log(getIndexsForPalindrome('aaaaaaaa')) // []
-console.log(getIndexsForPalindrome('aaababa')) // [1, 3]
-console.log(getIndexsForPalindrome('caababa')) // null
+console.log('result', getIndexsForPalindrome('anna')) // []
+console.log('result', getIndexsForPalindrome('abab')) // [0, 1]
+console.log('result', getIndexsForPalindrome('abac')) // null
+console.log('result', getIndexsForPalindrome('aaaaaaaa')) // []
+console.log('result', getIndexsForPalindrome('aaababa')) // [1, 3]
+console.log('result', getIndexsForPalindrome('caababa')) // null
 
 function getIndexsForPalindrome(word: string) {
   if (word === word.split('').reverse().join('')) {
     return []
   }
-  return [0, 0]
+
+  for (let i = 0; i < word.length; i++) {
+    for (let j = 0; j < word.length; j++) {
+      let wordArray = word.split('')
+      let temp = wordArray[i]
+      wordArray[i] = wordArray[j]
+      wordArray[j] = temp
+      if (wordArray.join('') === wordArray.reverse().join('')) {
+        return [i, j]
+      }
+    }
+
+  }
+  return null
 }
